c7e90eda0c2b7fc7b26bda232bbfb8756a7d6734
ayboori <105356296+ayboori@users.noreply.github.com>
2024-11-10 04:52:33 +0900

    feat : Admin 사용자 검증
    - admin 토큰을 활용한 사용자 검증
    - 일반 사용자로 회원가입 후 권한 변경

---
+++ src/main/java/com/sparta/springsecurity/auth/application/dto/AdminRequestDto.java
@@ -0,0 +1,10 @@
+package com.sparta.springsecurity.auth.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminRequestDto {
+    private String adminToken;
+}
