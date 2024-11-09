e5a49dd84c131a4dcdeffb1bcb0a652777616064
ayboori <105356296+ayboori@users.noreply.github.com>
2024-11-10 02:01:55 +0900

    feat : 회원가입, 로그인 기능
    - spring security 적용 전
    - postgreSQL 연결

--- src/main/java/com/sparta/springsecurity/dto/LoginResponseDto.java
+++ src/main/java/com/sparta/springsecurity/dto/LoginResponseDto.java
@@ -1,5 +1,11 @@
 package com.sparta.springsecurity.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginResponseDto {
     private String token;
 }
